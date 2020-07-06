cbba326c674095af825df37e8811c19fb2bef067
sajalrs <sajalsatyal@gmail.com>
2020-07-05 22:16:10 -0500

    Added Data Model

--- app/src/main/java/com/makeshift/whattimeisitthere/WhenaboutListFragment.kt
+++ app/src/main/java/com/makeshift/whattimeisitthere/WhenaboutListFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.makeshift.whattimeisitthere.databinding.FragmentWhenaboutListBinding
+import com.makeshift.whattimeisitthere.databinding.ListItemTimeBinding
 
-class WhenaboutListFragment: Fragment(){
+class WhenaboutListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,13 +20,39 @@ class WhenaboutListFragment: Fragment(){
     ): View? {
 
         val binding: FragmentWhenaboutListBinding =
-            DataBindingUtil.inflate(inflater,R.layout.fragment_whenabout_list, container,false)
+            DataBindingUtil.inflate(inflater, R.layout.fragment_whenabout_list, container, false)
 
-        binding.recyclerView.apply{
+        binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         }
 
         return binding.root
+    }
+
+    private inner class WhenaboutAdapter(var whenabouts: List<Whenabout>) :
+        RecyclerView.Adapter<WhenaboutHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+                WhenaboutHolder {
+            val binding = DataBindingUtil.inflate<ListItemTimeBinding>(
+                layoutInflater,
+                R.layout.list_item_time,
+                parent,
+                false
+            )
+            return WhenaboutHolder(binding)
+        }
+
+        override fun getItemCount(): Int = 0
+
+        override fun onBindViewHolder(holder: WhenaboutHolder, position: Int) {
+        }
+
+    }
+
+
+    private inner class WhenaboutHolder(private val binding: ListItemTimeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
     }
 
 }
