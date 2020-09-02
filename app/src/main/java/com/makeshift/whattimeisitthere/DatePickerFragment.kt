ef4903958d7fccf776be2eb0ae4f92bc9705c8f6
sajalrs <sajalsatyal@gmail.com>
2020-09-01 22:35:48 -0500

    DatePicker Reflects whenabout.dob

--- app/src/main/java/com/makeshift/whattimeisitthere/DatePickerFragment.kt
+++ app/src/main/java/com/makeshift/whattimeisitthere/DatePickerFragment.kt
@@ -3,17 +3,19 @@ package com.makeshift.whattimeisitthere
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
-private const val ARG_DATE="date"
-class DatePickerFragment: DialogFragment() {
+private const val ARG_DATE = "date"
+
+class DatePickerFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val calendar = Calendar.getInstance();
-        val initialYear = calendar.get(Calendar.YEAR);
-        val initialMonth = calendar.get(Calendar.MONTH);
-        val initialDay = calendar.get(Calendar.DAY_OF_MONTH);
+        val date = arguments?.getSerializable(ARG_DATE) as Date
+        val calendar = Calendar.getInstance()
+        calendar.time = date;
+        val initialYear = calendar.get(Calendar.YEAR)
+        val initialMonth = calendar.get(Calendar.MONTH)
+        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
 
         return DatePickerDialog(
             requireContext(),
@@ -23,15 +25,14 @@ class DatePickerFragment: DialogFragment() {
             initialDay
         )
     }
-    
-    companion object{
+
+    companion object {
         fun newInstance(date: Date): DatePickerFragment {
-            Log.i(ARG_DATE, date.toString())
             val args = Bundle().apply {
                 putSerializable(ARG_DATE, date)
             }
 
-            return DatePickerFragment().apply{
+            return DatePickerFragment().apply {
                 arguments = args
             }
         }
