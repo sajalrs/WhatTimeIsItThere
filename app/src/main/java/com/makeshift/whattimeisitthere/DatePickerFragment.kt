e197f065c554a198d4d2e26fc828dbb2857efe49
sajalrs <sajalsatyal@gmail.com>
2020-09-01 22:26:49 -0500

    Added Fragment Arguments to DatePicker

--- app/src/main/java/com/makeshift/whattimeisitthere/DatePickerFragment.kt
+++ app/src/main/java/com/makeshift/whattimeisitthere/DatePickerFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
+private const val ARG_DATE="date"
 class DatePickerFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendar = Calendar.getInstance();
@@ -20,5 +21,17 @@ class DatePickerFragment: DialogFragment() {
             initialMonth,
             initialDay
         )
+    }
+    
+    companion object{
+        fun newInstance(date: Date): {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+
+            return DatePickerFragment().apply{
+                arguments = args
+            }
+        }
     }
 }
