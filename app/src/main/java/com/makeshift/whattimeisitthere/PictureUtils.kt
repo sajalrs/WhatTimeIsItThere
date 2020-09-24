592982d30c40bfcec64e3f70ca854de31c75efc8
sajalrs <sajalsatyal@gmail.com>
2020-09-23 23:47:54 -0500

    Added Function to scale bitmap according to screen dimensions

--- app/src/main/java/com/makeshift/whattimeisitthere/PictureUtils.kt
+++ app/src/main/java/com/makeshift/whattimeisitthere/PictureUtils.kt
@@ -1,7 +1,9 @@
 package com.makeshift.whattimeisitthere
 
+import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Point
 
 class PictureUtils {
     fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int):
@@ -35,6 +37,12 @@ class PictureUtils {
         options.inSampleSize = inSampleSize
 
         return BitmapFactory.decodeFile(path, options)
+    }
+
+    fun getScaledBitmap(path: String,activity: Activity): Bitmap{
+        val size = Point()
+        activity.windowManager.defaultDisplay.getSize(size)
+        return getScaledBitmap(path, size.x, size.y)
     }
 
 
